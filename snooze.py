c1acd68b3a206574a3a6a1ed66dbfba5bb27ff0e
adibch <143553724+adibch@users.noreply.github.com>
2023-08-29 15:41:59 -0500

    Add files via upload

---
+++ snooze.py
@@ -0,0 +1,24 @@
+# Nathan Moore
+# starting from from adafruit example
+# https://learn.adafruit.com/welcome-to-circuitpython/creating-and-editing-code
+#
+import board
+import digitalio
+import time
+# set up the (red) LED
+led = digitalio.DigitalInOut(board.D13)
+led.direction = digitalio.Direction.OUTPUT
+while True:
+    for brightness in [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0]:
+        # brightness is measured out of a max of 1.0
+        T_fast = 0.01
+        T_on = brightness*T_fast
+        T_off = (1-brightness)*T_fast
+        num_repeats=7
+        i=0
+        while (i<num_repeats):
+            led.value = True
+            time.sleep(T_on)
+            led.value = False
+            time.sleep(T_off)
+            i=i+1
